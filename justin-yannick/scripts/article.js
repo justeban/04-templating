c732e3600b02dee227c4f3113afb225d6eb00200
Justin Morris <justinjodymorris@gmail.com>
2018-04-13 09:53:52 -0700

    succesfully implemented Handlbars.js

--- justin-yannick/scripts/article.js
+++ justin-yannick/scripts/article.js
@@ -13,7 +13,8 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -31,11 +32,11 @@ Article.prototype.toHtml = function() {
   // }
 
   // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  return template(this);
 
 };
-
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// Specific to the sort function, the sort function is taking in, as an input, another function with two parameters.
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
